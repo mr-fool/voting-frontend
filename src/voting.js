9cfa017dc5bd88247b706d11b5d1b3fdd813cde8
mr-fool <mr-fool@users.noreply.github.com>
2019-06-16 12:32:34 -0600

    cleaning abi fail

--- src/voting.js
+++ src/voting.js
@@ -2,69 +2,69 @@ const address = "0xdfbf946546cac92e0cca7ad3d85697781873a014";
 
 const abi : [
     {
-      "constant": true,
-      "inputs": [],
-      "name": "normie",
-      "outputs": [
+      constant: true,
+      inputs: [],
+      name: "normie",
+      outputs: [
         {
-          "name": "",
-          "type": "uint256"
+          name: "",
+          type: "uint256"
         }
       ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
+      payable: false,
+      stateMutability: "view",
+      type: "function"
     },
     {
-      "constant": true,
-      "inputs": [],
-      "name": "robot",
-      "outputs": [
+      constant: true,
+      inputs: [],
+      name: "robot",
+      outputs: [
         {
-          "name": "",
-          "type": "uint256"
+          name: "",
+          type: "uint256"
         }
       ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
+      payable: false,
+      stateMutability: "view",
+      type: "function"
     },
     {
-      "constant": true,
-      "inputs": [
+      constant: true,
+      inputs: [
         {
-          "name": "",
-          "type": "address"
+          name: "",
+          type: "address"
         }
       ],
-      "name": "voted",
-      "outputs": [
+      name: "voted",
+      outputs: [
         {
-          "name": "",
-          "type": "bool"
+          name: "",
+          type: "bool"
         }
       ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
+      payable: false,
+      stateMutability: "view",
+      type: "function"
     },
     {
-      "constant": false,
-      "inputs": [],
-      "name": "voteNormie",
-      "outputs": [],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "function"
+      constant: false,
+      inputs: [],
+      name: "voteNormie",
+      outputs: [],
+      payable: false,
+      stateMutability: "nonpayable",
+      type: "function"
     },
     {
-      "constant": false,
-      "inputs": [],
-      "name": "voteRobot",
-      "outputs": [],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "function"
+      constant: false,
+      inputs: [],
+      name: "voteRobot",
+      outputs: [],
+      payable: false,
+      stateMutability: "nonpayable",
+      type: "function"
     }
   ];
 
