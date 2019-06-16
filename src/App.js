be7d1c20b620e958a6641a46b6dd4dbdf6f348cf
mr-fool <mr-fool@users.noreply.github.com>
2019-06-16 13:05:35 -0600

    int

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Web3 from "web3";
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import contractData from "./voting";
+import Web3 from "web3";
+const web3 = new Web3(window.web3.currentProvider);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+     web3: {}, 
+     players: [],
+     normie: int,
+     robot: int
+  };
+  async componentDidMount() {
+    console.log(contractData);
+    const voting = new web3.eth.Contract(
+      contractData.abi,
+      contractData.address
+    );
+  }
 }
-
-export default App;
+  export default App;
