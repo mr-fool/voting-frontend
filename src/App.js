babb342c81f4704ca69421ff9443574028f0dd49
mr-fool <mr-fool@users.noreply.github.com>
2019-06-16 12:23:38 -0600

    copying the abi and contract address

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import Web3 from "web3";
 
-const contractAddress = 0xdfbf946546cac92e0cca7ad3d85697781873a014;
-
 function App() {
   return (
     <div className="App">
---
+++ src/voting.js
@@ -0,0 +1,74 @@
+const address = "0xdfbf946546cac92e0cca7ad3d85697781873a014";
+
+const abi : [
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "normie",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [],
+      "name": "robot",
+      "outputs": [
+        {
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": true,
+      "inputs": [
+        {
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "voted",
+      "outputs": [
+        {
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [],
+      "name": "voteNormie",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "constant": false,
+      "inputs": [],
+      "name": "voteRobot",
+      "outputs": [],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ];
+
+  export default {
+    abi,
+    address
+  };
